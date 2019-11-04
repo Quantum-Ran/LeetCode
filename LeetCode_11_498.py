bc73a93008e6c7e044a85c2e40431bcb0c973613
Quantum-Ran <46480626+Quantum-Ran@users.noreply.github.com>
2019-11-04 10:42:49 +0800

    2019年11月4日

--- LeetCode_11_498.py
+++ LeetCode_11_498.py
@@ -25,21 +25,26 @@
 
 class Solution:
     def maxArea(self, height):
+        """
+        方法？双指针，分为左右，然后不断往里移动压缩，直到相邻，但不能相等。
+        移动谁？左右那个高度小就移动谁。
+        面积？底 * 高 = 左右两个指针的差 * 当前的最小的高度。
+        """
         # 初始化最大盛水量，用于储存
         max_area = 0
         # 双指针
-        left_index = 0
-        right_index = len(height) - 1
+        left_ids = 0
+        right_ids = len(height) - 1
         # 当左指针不等于右指针，因为两边都往里走，所以最终肯定会相遇
-        while left_index != right_index:
+        while left_ids != right_ids:
             # 左右那边矮，就移动那边指针向里，看看能否遇到大的
-            if height[left_index] < height[right_index]:
+            if height[left_ids] < height[right_ids]:
                 # 底部等于两个索引的差
-                max_area = max(height[left_index] * (right_index - left_index), max_area)
-                left_index += 1
+                max_area = max(height[left_ids] * (right_ids - left_ids), max_area)
+                left_ids += 1
             else:
-                max_area = max(height[right_index] * (right_index - left_index), max_area)
-                right_index -= 1
+                max_area = max(height[right_ids] * (right_ids - left_ids), max_area)
+                right_ids -= 1
         return max_area
 
 
